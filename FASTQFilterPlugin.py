5ca7111caed26dc7b78a86cb4ff9d6853d0a8e1b
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2022-06-30 08:50:09 -0400

    Initial

---
+++ FASTQFilterPlugin.py
@@ -0,0 +1,37 @@
+import sys
+import PyPluMA
+class FASTQFilterPlugin:
+    def input(self, inputfile):
+       infile = open(inputfile, 'r')
+       params = dict()
+       for line in infile:
+           contents = line.strip().split('\t')
+           params[contents[0]] = contents[1]
+       self.fastqfile = open(PyPluMA.prefix()+"/"+params["fastqfile"], 'r')
+       self.samfile = open(PyPluMA.prefix()+"/"+params["samfile"], 'r')
+
+    def run(self):
+       self.aligned = set()
+       for line in self.samfile:
+          seqname = line.strip().split('\t')[0]
+          self.aligned.add(seqname)
+
+    def output(self, outfile):
+       outfastq = open(outfile, 'w')
+
+       pos = 0
+       for line in self.fastqfile:
+          if (pos % 4 == 0):
+              lines = []
+              lines.append(line)
+              seqname = line.strip().split(' ')[0]
+              seqname = seqname[1:]
+          elif (pos % 4 == 3):
+              lines.append(line)
+              if (seqname not in self.aligned):
+                  for i in range(0, 4):
+                      outfastq.write(lines[i])
+          else:
+              lines.append(line)
+          pos += 1
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # FASTQFilter
-Remove sequences from a FASTQ file, using a SAM filter
+# Language: Python
+# Input: TXT
+# Output: FASTQ
+# Tested with: PluMA 1.1, Python 3.6
+
+PluMA plugin that takes a FASTQ file and filters out
+sequences present in an alignment (SAM) file.
+
+This is useful when for example removing host data
+from a sample.
+
+The plugin accepts as input a TXT file of keyword-value pairs,
+tab-delimited:
+
+fastqfile: Input FASTQ file
+samfile: Input SAM filter
+
+Output will be sent to the user-specified FASTQ.
